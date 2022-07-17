fb5b154b903eb1308b4c3b0f5790e66935785c2a
Ben Kiboma <omayioben@gmail.com>
2022-07-17 15:51:20 +0300

    Create product reducer

---
+++ src/Redux/Actions/productActions.js
@@ -0,0 +1,16 @@
+// Create 3 actions for set product, select product and deselect product
+import { ActionTypes } from "../Constants/action-types"
+
+export const setProducts = (products) => {
+  return {
+    type: ActionTypes.SET_PRODUCTS,
+    payload: products,
+  }
+}
+
+export const selectedProducts = (product) => {
+  return {
+    type: ActionTypes.SELECTED_PRODUCT,
+    payload: product,
+  }
+}
