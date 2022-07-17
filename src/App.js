4083e4c1e03632d5949685b8a40a29837937f379
Ben Kiboma <omayioben@gmail.com>
2022-07-17 17:58:55 +0300

    Use Routers to display different pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import {
+  BrowserRouter as Router, Routes, Route,
+} from 'react-router-dom';
 import './App.css';
 import Header from './Containers/Header';
+import ProductDetails from './Containers/ProductDetails';
+import ProductListing from './Containers/productListings';
 
-function App() {
-  return (
-    <div className="App">
-        <Header />
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Header />
+    <Routes>
+      <Route path="/" element={<ProductListing />} />
+      <Route path="/product/:productid" element={<ProductDetails />} />
+      <Route>404 Not Found</Route>
+    </Routes>
+  </Router>
+)
 
 export default App;
