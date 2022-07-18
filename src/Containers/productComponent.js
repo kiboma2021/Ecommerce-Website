cc9f957cf61c0f49094cf557206b3d84272d997a
Ben Kiboma <omayioben@gmail.com>
2022-07-18 11:13:49 +0300

    Map the poducts on the main page

--- src/Containers/productComponent.js
+++ src/Containers/productComponent.js
@@ -3,12 +3,18 @@ import { useSelector } from "react-redux";
 
 const ProductComponent = () => {
   const products = useSelector((state) => state.allproducts.products );
- // const { id, title } =products[0]
-  return (
-    <div>
-      <p>{ /* title */}</p>
+  const renderList = products.map((product) => {
+    const {id, title, image, price, category} = product;
+    return (
+      <div key={id}>
+        <div><img src={image} alt={title} /></div>
+        <div>{ title }</div>
+        <div>${ price }</div>
+        <div>{ category }</div>
     </div>
-  )
+    );
+  });
+  return <>{renderList}</>
 }
 
 export default ProductComponent;
