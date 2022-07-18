0caf2bffe5c968d26d3bdba4e63eea3c3fbebd02
Ben Kiboma <omayioben@gmail.com>
2022-07-18 11:33:45 +0300

    Style using semantic UI

--- src/Containers/productComponent.js
+++ src/Containers/productComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const ProductComponent = () => {
@@ -6,11 +7,21 @@ const ProductComponent = () => {
   const renderList = products.map((product) => {
     const {id, title, image, price, category} = product;
     return (
-      <div key={id}>
-        <div><img src={image} alt={title} /></div>
-        <div>{ title }</div>
-        <div>${ price }</div>
-        <div>{ category }</div>
+      <div className="four wide column" key={id}>
+        <Link to={`/product/${id}`}>
+          <div className="ui link cards">
+            <div className="card">
+              <div className="image">
+                <img src={image} alt={title} />
+              </div>
+              <div className="content">
+                <div className="header">{ title }</div>
+                <div className="meta price">${ price }</div>
+                <div className="meta">{ category }</div>
+              </div>
+            </div>
+          </div>
+        </Link>
     </div>
     );
   });
